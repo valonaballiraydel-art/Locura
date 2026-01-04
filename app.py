25f54c9adcf14b7530fc437563d149a87ef00d2e
valonaballiraydel-art <valonaballiraydel@gmail.com>
2026-01-04 16:23:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from utils import wait_for_table
+from utils import wait_for_table, download_geckodriver
 
 def main():
-    # Configurar Firefox
-    options = Options()
-    options.set_preference("network.proxy.type", 0)
-    options.headless = True  # importante para Render
-
-    driver = webdriver.Firefox(options=options)
+    # Descargar geckodriver automáticamente
+    driver_path = download_geckodriver()
     
+    # Configurar Firefox headless
+    options = Options()
+    options.headless = True
+
+    driver = webdriver.Firefox(options=options, executable_path=driver_path)
+
     try:
         url = "https://int.soccerway.com/national/spain/primera-division/"
         driver.get(url)
 
         tabla, filas = wait_for_table(driver)
-
         print(f"Filas encontradas: {len(filas)}")
+
         for fila in filas:
             columnas = fila.find_elements("tag name", "td")
             datos = [columna.text for columna in columnas]
             print(datos)
-    
+
     finally:
         driver.quit()
 
 if __name__ == "__main__":
     main()
+
